5795ca4b7b93f864c83216a071087d2d699e0828
Snehasish Paul <weishenpov@gmail.com>
2024-10-08 02:11:02 +0530

    initial commit
    initial commit

---
+++ src/main/java/com/snehasish/microservice/question/QuizAppQuestionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.snehasish.microservice.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppQuestionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppQuestionMicroserviceApplication.class, args);
+	}
+
+}
